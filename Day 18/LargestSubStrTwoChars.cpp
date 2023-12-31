081018a1b0bf43d8c8d20197bf260bfd1bd2e740
Sanket Shendge <105985549+SanketShendge21@users.noreply.github.com>
2023-12-31 19:24:04 +0530

    Add files via upload

---
+++ Day 18/LargestSubStrTwoChars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        vector<int> alpha(26, -1);
+        int n=s.size(), maxLen=-1;
+         for (int i = 0; i < n; i++) {
+            int idx = s[i]-'a';
+            if (alpha[idx]!= -1){
+                maxLen= max(maxLen, i-alpha[idx]-1);
+            }
+            else alpha[idx]=i;
+        } 
+        return maxLen;
+    }
+};
