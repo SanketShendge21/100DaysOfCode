2b74e67287bc3adc852531e39fb1c1946e74cc87
Sanket Shendge <105985549+SanketShendge21@users.noreply.github.com>
2024-02-14 12:23:39 +0530

    Rearrange Array Elements by Sign

---
+++ Day 63/RearrangeArrayElementsbySign.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> a, b, c;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > 0)
+                a.push_back(nums[i]);
+            else
+                b.push_back(nums[i]);
+        }
+
+        int count = 0, c1 = 0, c2 = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(count % 2 == 0) {
+                int n = a[c1++];
+                c.push_back(n);
+            } else {
+                int n = b[c2++];
+                c.push_back(n);
+            }
+            count++;
+        }
+
+        return c;
+    }
+};
