e23462e6b2e72521abea3b4a6495b2fa0130b940
Sanket Shendge <105985549+SanketShendge21@users.noreply.github.com>
2024-02-25 11:23:21 +0530

    "Subset II"
    Find unique subsets in a given array

---
+++ Day 74/SubsetII.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void dfs(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& result) {
+        result.push_back(curr);
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            curr.push_back(nums[i]);
+            dfs(nums, i + 1, curr, result);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+        vector<int> curr;
+        vector<vector<int>> result;
+        dfs(nums, 0, curr, result);
+        return result;
+    }
+};
